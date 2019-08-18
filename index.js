4dea0803c67d928f945ba3b35a33cbc04e27ccc2
Arjun <arjungupta2110@gmail.com>
2019-08-18 04:08:27 +0100

    design work

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
-$("body").css("display", "none");
+function smoothScrollingOfNav() {
+  // https://www.w3schools.com/howto/howto_css_smooth_scroll.asp#section2
+  $(".nav-link").on("click", function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash == "") {
+      return;
+    }
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes
+    // to scroll to the specified area
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top
+        // scrollBottom: $(hash).offset().bottom
+      },
+      1000,
+      function() {}
+    );
+  });
+}
+
+function collapseNavMenuOnLinkClick() {
+  $(".nav-link").on("click", function() {
+    $(".navbar-collapse").collapse("hide");
+  });
+}
 
 $(document).ready(function() {
+  collapseNavMenuOnLinkClick();
+  smoothScrollingOfNav();
+
+  // load logo
   $("#logo-container").load("./assets/logo.svg", function() {
-    $("body").css("display", "block");
+    // render page
+    $("body").animate({ opacity: 1 }, 600);
   });
 });
