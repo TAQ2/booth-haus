39d29f9fa9ec1ca36619db81e99b0e2c8383381c
Arjun <arjungupta2110@gmail.com>
2019-09-04 14:31:28 +0100

    Clean up nav bar

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-function smoothScrollingOfNav() {
-  // https://www.w3schools.com/howto/howto_css_smooth_scroll.asp#section2
-  $(".nav-link").on("click", function(event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash == "") {
-      return;
-    }
-    // Prevent default anchor click behavior
-    event.preventDefault();
-
-    // Store hash
-    var hash = this.hash;
-
-    // Using jQuery's animate() method to add smooth page scroll
-    // The optional number (800) specifies the number of milliseconds it takes
-    // to scroll to the specified area
-    $("html, body").animate(
-      {
-        scrollTop: $(hash).offset().top
-        // scrollBottom: $(hash).offset().bottom
-      },
-      1000,
-      function() {}
-    );
-  });
-}
-
 function collapseNavMenuOnLinkClick() {
   $(".nav-link").on("click", function() {
     $(".navbar-collapse").collapse("hide");
@@ -33,7 +6,7 @@ function collapseNavMenuOnLinkClick() {
 
 $(document).ready(function() {
   collapseNavMenuOnLinkClick();
-  smoothScrollingOfNav();
+  // smoothScrollingOfNav();
 
   $("body").animate({ opacity: 1 }, 600);
 
